99e38d4e14be9fe4667396b0f52a057cbc82de4c
Liam Keyek <102824375+liamkeyek@users.noreply.github.com>
2024-08-01 16:20:11 -0600

    db

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask
 app = Flask(__name__)
+import psycopg2
 
 @app.route('/')
 def hello_world():
     return 'Hello world from Liam in 3308!'
 
+@app.route('/db_test')
+def db_test():
+    try:
+        conn = psycopg2.connect("postgresql://liam_lab_10_postgresql_db_user:ZPqw3tqzwtOjm6kAVf6EzlKPRjYLaiL1@dpg-cqm0g5qj1k6c73e1h1ig-a/liam_lab_10_postgresql_db")
+        conn.close()
+        return "Database Connection Successful"
+    except Exception as e:
+        return f"An error occurred: {e}"
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
